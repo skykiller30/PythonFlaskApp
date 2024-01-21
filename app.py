d86f53e54e0db3c898e4574310cb2bac8ff315f3
skykiller30 <lukeskykiller30@gmail.com>
2024-01-21 23:40:39 +0000

    time fix bak to UTC

--- app.py
+++ app.py
@@ -7,11 +7,13 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
-class Todo(db.Model): 
+class Todo(db.Model):
+
+
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
     completed = db.Column(db.Integer, default=0)
-    date_created = db.Column(db.Text, default=datetime.now(tz=pytz.timezone("US/Central")).strftime('%m-%d-%Y %I:%M:%S %p'))
+    date_created = db.Column(db.DateTime, default=datetime.now)
     
 
 def __repr__(self):
